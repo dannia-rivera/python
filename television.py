a74e5ac7afe15ddd12fdfdd0b2aac3398fd92dd5
Dannia <danniarivera035@gmail.com>
2024-04-23 21:48:58 -0500

    made some changes to make sure the program runs smoothly

--- television.py
+++ television.py
@@ -1,67 +1,93 @@
 class Television:
     #class representing television
 
-    min_volume = 0
-    max_volume = 20
-    min_channel = 0
-    max_channel = 100
+    min_volume: int = 0
+    max_volume: int = 20
+    min_channel:int  = 0
+    max_channel: int = 100
 
-    def __init__(self):
-        #setting up default values
-
+    def __init__(self) -> None:
+        '''
+        setting up default values
+        '''
         self.__status = False
         self.__muted = False
-        self.__volume = self.min_volume
-        self.__channel = self.min_channel
+        self.__volume: int = Television.min_volume
+        self.__channel: int = Television.min_channel
 
     def power(self):
-        #setting for the television on/off
+        '''
+        setting for the television on/off
+        '''
+        if self.__status == False:
+            self.__status = True
+        else:
+            self.__status = False
 
-        self.__status = not self.__status
-
-    def mute(self):
-        #mute and unmute setting
+    def mute(self) -> None:
+        '''
+        This function mutes and unmutes the television when its on
+        '''
         if self.__status:
-            self.__muted = not self.__muted
-
-    def channel_up(self):
-        #tv channel goes up
-
-        if self.__status:
-            if self.__channel == self.max_channel:
-                self.__channel = self.min_channel
+            if self.__muted is False:
+                self.__muted = True
             else:
-                self.channel += 1
+                self.__muted = False
+        else:
+            self.__volume = Television.min_volume
 
-    def channel_down(self):
-        #tv channel goes down
 
+
+    def channel_up(self) -> None:
+        '''
+        This function makes tv channel go up when the tv in on
+        '''
         if self.__status:
-            if self.__channel == self.min_channel:
-                self.__channel = self.max_channel
+            if self.__channel < Television.max_channel:
+                self.__channel += 1
             else:
+                self.channel = Television.min_channel
+
+    def channel_down(self) -> None:
+        '''
+        This function makes tv channel go down when the tv in on
+        '''
+        if self.__status:
+            if self.__channel > Television.min_channel:
                 self.channel -= 1
+            else:
+                self.channel = Television.max_channel
 
-    def volume_up(self):
-        #this increases the volume for the tv
-
+    def volume_up(self) -> None:
+        '''
+        this function increases the volume for the tv and unmutes the tv
+        '''
         if self.__status:
-            if self.__volume < self.max_volume:
+            self.__muted = False
+            if self.__volume < Television.max_volume:
                 self.__volume += 1
-                self.__muted = False
+            else:
+                pass
 
-    def volume_down(self):
-        #decrease tv volume
-
+    def volume_down(self) -> None:
+        '''
+        This function decrease tv volume and unmutes if tv is muted
+        '''
         if self.__status:
-            if self.__volume > self.min_volume:
+            self.__muted = False
+            if self.__volume > Television.min_volume:
                 self.__volume -= 1
-                self.__muted = False
+            else:
+                pass
 
     def __str__(self):
-        #returning string to tv
-
-        return f'Power={self.__status}, Channel={self.__channel},Volume={self.__volume}'
+        '''
+        This function makes sure that if the tv is mutes it returns the values like power, channel, and volume
+        '''
+        if self.__muted:
+            return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {Television.min_volume}'
+        else:
+            return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
 
 
 
--- test.py
+++ test.py
@@ -4,4 +4,88 @@ class Test:
     def setup_method(self):
         self.tele1 = Television()
 
-    def 
+    def teardown_method(self):
+        assert self.tele1.__str__() == 'Power = False, Channel = 0, Volume = 0'
+
+        def test_power(self):
+            self.tele1.power()
+            assert self.tele1__str__() == 'Power = True, Channel = 0, Volume = 0'
+            self.tele1.power()
+            assert self.tele1__str__() == 'Power = False, Channel = 0, Volume = 0'
+            self.tele1.power()
+            assert self.tele1__str__() == 'Power = True, Channel = 0, Volume = 0'
+
+        def test_mute(self):
+            assert self.tele1.__str__() == 'Power = False, Channel = 0, Volume = 0'
+
+            self.tele1.power()
+            self.tele1.volume_up()
+            self.tele1.mute()
+            assert self.tele1__str__() == 'Power = True, Channel = 0, Volume = 0'
+
+            self.tele1.mute()
+            assert self.tele1__str__() == 'Power = True, Channel = 0, Volume = 1'
+
+            self.tele1.power()
+            self.tele1.mute()
+            assert self.tele1__str__() == 'Power = False, Channel = 0, Volume = 0'
+
+            self.tele1.mute()
+            assert self.tele1__str__() == 'Power = False, Channel = 0, Volume = 0'
+
+        def test_channel_up(self):
+            self.tele1.channel_up()
+            assert self.tele1__str__() == 'Power = False, Channel = 0, Volume = 0'
+
+            self.tele1.power()
+            self.tele1.channel_up()
+            assert self.tele1__str__() == 'Power = True, Channel 1, Volume = 0'
+
+            self.tele1.channel_up()
+            self.tele1.channel_up()
+            self.tele1.channel_up()
+            assert self.tele1__str__() == 'Power = True, Channel = 0, Volume = 0'
+
+        def test_channel_down(self):
+            self.tele1.channel_down()
+            assert self.tele1__str__() == 'Power = False, Channel = 0, Volume = 0'
+
+            self.tele1.power()
+            self.tele1.channel_down()
+            assert self.tele1__str__() == 'Power = True, Channel = 3, Volume = 0'
+
+            self.tele1.channel_down()
+            assert self.tele1__str__() == 'Power = True, Channel = 2, Volume = 0'
+
+        def test_volume_up(self):
+            self.tele1.volume_up()
+            assert self.tele1__str__() == 'Power = False, Channel = 0, Volume = 0'
+
+            self.tele1.power()
+            self.tele1.volume_up()
+            assert self.tele1__str__() == 'Power = True, Channel = 0, Volume = 1'
+
+            self.tele1.mute()
+            self.tele1.volume_up()
+            assert self.tele1__str__() == 'Power = True, Channel = 0, Volume = 2'
+
+            self.tele1.volume_up()
+            self.tele1.volume_up()
+            assert self.tele1__str__() == 'Power = True, Channel = 0, Volume = 2'
+
+            self.tele1.mute()
+            assert self.tele1__str__() == 'Power = True, Channel = 0, Volume = 0'
+
+        def test_volume_down(self):
+            self.tele1.volume_down()
+            assert self.tele1__str__() == 'Power = False, Channel =0, Volume = 0'
+
+            self.tele1.power()
+            self.tele1.volume_up()
+            self.tele1.volume_up()
+            self.tele1.volume_down()
+            assert self.tele1__str__() == 'Power = True, Channel = 0, Volume = 1'
+
+            self.tele1.mute()
+            self.tele1.volume_down()
+            assert self.tele1__str__() == 'Power = True, Channel = 0, Volume = 0'
